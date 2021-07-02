e460f4f2f5d4b9458d3866a89ae6c63e0ab823e2
Michael Ziegler <michael.ziegler@proemion.com>
2021-07-02 15:59:40 +0200

    indent

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
 set -e
 set -u
 
 (
-	cd cmd/win-dns-api-go
-        GOOS=windows go build
+    cd cmd/win-dns-api-go
+    GOOS=windows go build
 )
 
 (
-	cd cmd/win-dns-to-bind
-        go build
-        GOOS=windows go build
+    cd cmd/win-dns-to-bind
+    go build
+    GOOS=windows go build
 )
 
 cp cmd/win-dns-api-go/win-dns-api-go.exe .
